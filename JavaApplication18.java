d9a34ba3321e440e17cea81291fcafd80f157470
mingjhen <102316110@gms.tcu.edu.tw>
2016-02-29 22:28:08 +0800

    七言絕句

---
+++ JavaApplication18.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication18;
+
+/**
+ *
+ * @author MingJhen
+ */
+public class JavaApplication18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("送孟浩然之廣陵");
+        System.out.println("作者：李白\n");
+        System.out.println("故人西辭黃鶴樓");
+        System.out.println("煙花三月下揚州");
+        System.out.println("孤帆遠影碧空盡");
+        System.out.println("惟見長江天際流");
+
+    }
+    
+}
